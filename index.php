da136bfa354c737e498eecf035b2f6d0496a3e1c
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2024-07-23 15:39:58 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+Kirby::plugin('vv/collection-manager', [
+
+  'snippets' => [
+    'collection-pagination' => __DIR__ . '/snippets/collection-pagination.php',
+  ],
+
+]);
---
+++ snippets/collection-pagination.php
@@ -0,0 +1,33 @@
+<?php $pagination = $collection->pagination() ?>
+<nav class='collection-pagination'>
+  <ul>
+    <?php if ($pagination->hasPrevPage()) : ?>
+    <li class="collection-pagination__item collection-pagination__item--to-sibling">
+      <a href="<?php echo $pagination->prevPageURL() ?>">
+        <span class="collection-pagination__icon collection-pagination__icon--prev"></span>
+      </a>
+    </li>
+    <?php endif ?>
+
+    <?php foreach ($pagination->range($range ?? 10) as $r) : ?>
+    <li class="collection-pagination__item collection-pagination__item--to-number">
+      <a <?php
+      echo attr([
+        'href' => $pagination->pageURL($r),
+        'aria-current' => $pagination->page() === $r ? 'page' : null,
+      ])
+          ?>>
+      <?php echo $r ?>
+      </a>
+    </li>
+    <?php endforeach ?>
+
+    <?php if ($pagination->hasNextPage()) : ?>
+    <li class="collection-pagination__item collection-pagination__item--to-sibling">
+      <a href="<?php echo $pagination->nextPageURL() ?>">
+        <span class="collection-pagination__icon collection-pagination__icon--next"></span>
+      </a>
+    </li>
+    <?php endif ?>
+  </ul>
+</nav>
